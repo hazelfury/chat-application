be72041d391f3102f488f6950af47b9dc5c8178d
hazelfury <55709967+hazelfury@users.noreply.github.com>
2026-07-02 17:36:07 -0400

    Week 2: User registration and Django Auth login

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('room/<slug:room_slug>/', views.room, name='room'),
     path('create-room/', views.create_room, name='create_room'),
+    path('register/', views.register, name='register'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils.text import slugify
 from .models import Room
+from .forms import RegisterForm
+from django.contrib.auth import login
 
 @login_required
 def index(request):
@@ -23,3 +25,16 @@ def create_room(request):
             Room.objects.get_or_create(slug=slug, defaults={'name': name, 'description': description})
             return redirect('room', room_slug=slug)
     return render(request, 'chat/create_room.html')
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'chat/register.html', {'form': form})
